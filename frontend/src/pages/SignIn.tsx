d68f400f264de458e977ce877b8a8fbe9035a219
Anhtu1110 <132809913+Anhtu1110@users.noreply.github.com>
2024-06-06 09:55:22 +0700

    Update SignIn.tsx

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,8 +1,8 @@
 import { useForm } from "react-hook-form";
-import { useMutation, } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
-
+import { useNavigate } from "react-router-dom";
 
 
 export type SignInFormData = {
@@ -18,7 +18,7 @@ const SignIn = () => {
     const mutation = useMutation(apiClient.signIn, {
         onSuccess: async() => {
             showToast({ message:"Sign in successful", type: "success"})
-            await queryClient.validate
+            await queryClient.invalidateQueries("validate token")
         },
         onError: (error: Error) => {
             showToast({ message: error.message, type: "error" });
